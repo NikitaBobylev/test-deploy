39d63260dfc9eb54d15c247a50f8672620e69604
Nikita Bobylev <nikitabobylev@MacBook-Pro-Nikita.local>
2024-07-29 06:38:14 +0300

    r

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import axios from 'axios'
 
 
-const backend_uri = 'https://api.nikitabobylev.site'
+const backend_uri = 'https://api.nikitabobylev.site/items'
 
 
 function App() {
